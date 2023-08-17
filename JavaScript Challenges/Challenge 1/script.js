2c5648ffef497ad642ba67bb431ad3ab5e46302f
Mohamad Safa <mohsafa3@gmail.com>
2023-08-17 10:59:42 +0300

    Challenge 1 - Done with Step 4

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -9,3 +9,22 @@ const lastname = 'Safa'
 console.log(lastname);
 var DOB = '01/09/1996';
 console.log(DOB);
+
+const string = "This is a text"
+console.log(string); // used for Strings
+let boolean1 = true
+console.log(boolean1);
+let boolean2 = false
+console.log(boolean2); // used for Booleans
+let number = 1215
+console.log(number); // used for numbers, without brackets, number displayed in different color
+const Number = "999"
+console.log(Number); // used for numbers, with brackets, numbers displayed as String color
+const person = {firstname: "Mohamad", lastname: "Safa"} // used for objects
+console.log(person);
+const Array1 = ["Red", "Blue", "Yellow", "Green"]
+console.log(Array1); // used for array
+let num1 = null
+console.log(null); //used for null
+let num2
+console.log(num2); //used for undefined value, when we don't put an equal
