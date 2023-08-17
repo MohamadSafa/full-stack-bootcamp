2aa3271d2f8ef0699aabce7786d958e10745325a
Mohamad Safa <mohsafa3@gmail.com>
2023-08-17 10:17:06 +0300

    Challenge 1 - Done with Step 3

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -2,3 +2,10 @@ console.log('I am linked now!');
 
 var name = 'Mohamad';
 alert('Mohamad');
+
+let age = '26'
+console.log(age);
+const lastname = 'Safa'
+console.log(lastname);
+var DOB = '01/09/1996';
+console.log(DOB);
