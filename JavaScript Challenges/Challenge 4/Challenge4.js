f5f221745c2f84b5818bc84ce38d9503ba7ad806
Mohamad Safa <mohsafa3@gmail.com>
2023-08-22 14:47:37 +0300

    Challenge 4 - Done with step 1

---
+++ JavaScript Challenges/Challenge 4/Challenge4.js
@@ -0,0 +1,15 @@
+//Step 1
+    //Array Creation
+
+let fiveNumbers = [10,20,30,40,50];
+console.log(fiveNumbers);
+
+    //Object Creation
+const personalInfo = {
+    name: "Mohamad",
+    age: 26,
+    height: "184 cm",
+    location: "Salim Slem",
+    nationality: "Lebanese",
+}
+console.log(personalInfo);
