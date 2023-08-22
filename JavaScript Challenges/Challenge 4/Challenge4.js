6ce948fce07590ea01435524432cda779f659e03
Mohamad Safa <mohsafa3@gmail.com>
2023-08-22 16:24:28 +0300

    Challenge 4 - Done with step 4

--- JavaScript Challenges/Challenge 4/Challenge4.js
+++ JavaScript Challenges/Challenge 4/Challenge4.js
@@ -1,4 +1,4 @@
-//Step 1
+//Step 1 : Arrays and Objects
     //Array Creation
 let fiveNumbers = [10,20,30,40,50];
 console.log(fiveNumbers);
@@ -13,7 +13,7 @@ const personalInfo = {
 }
 console.log(personalInfo);
 
-//Step 2
+//Step 2 : Spread Operator
     //New Array with Spread
 let nextThreeNumbers = [11,22,33];
 console.log(nextThreeNumbers);
@@ -32,7 +32,7 @@ console.log(additionalInfo);
 const fullInfo = {...personalInfo,...additionalInfo};
 console.log(fullInfo);
 
-//Step 3
+//Step 3 : Destructuring
     //Destructuring Arrays
 const [firstNum,secondNum,thirdNumb] = fiveNumbers;
 console.log(firstNum,secondNum,thirdNumb);
@@ -41,4 +41,33 @@ console.log(firstNum,secondNum,thirdNumb);
 const {name,age,height} = personalInfo;
 console.log(name,age,height);
 
+//Step 4 : Advanced Operations
+    //Nested Destructuring
+    const person = {
+        name1: 'Mohamad',
+        age1: 26,
+        personalInfo: {
+          contactInfo: {
+            email: 'mohsafa3@gmail.com',
+            phone: '03123456'
+          }
+        }
+      };
+      
+      const { name1, age1, personalInfo: { contactInfo: { email, phone } } } = person;
+      
+      console.log(name1);
+      console.log(age1);
+      console.log(email); 
+      console.log(phone);
+    
+      //Rest with Destructuring
+      const [firstNum1,secondNum1,...restOfNumbers] = fiveNumbers;
+      console.log(firstNum,secondNum,restOfNumbers);
 
+      //Merging and Overwriting with Spread
+      let fruits = ['Apple','Tomato','Banana','Cucumber'];
+      let vegetables = ['Lettuce','Cucumber','Onion','Tomato'];
+      const mergedObject = {...fruits,...vegetables};
+      console.log(mergedObject); //- This will show the last input on the console only.
+      
