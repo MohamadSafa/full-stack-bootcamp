2337370cd905ceeb957b540e9a623bb8d6af51b9
Mohamad Safa <mohsafa3@gmail.com>
2023-08-23 16:44:53 +0300

    Challenge 5 - Done with step 2

--- JavaScript Challenges/Challenge 5/script.js
+++ JavaScript Challenges/Challenge 5/script.js
@@ -31,3 +31,70 @@ for (let i = 0; i < heading.length; i++) {
     heading[i].style.fontStyle = 'italic';
 }
 
+//Step 2 : Event Handling
+    //Multiple Event Listeners
+const colorButton = document.getElementById('colorButton');
+colorButton.addEventListener ('mouseover', () => {
+    colorButton.style.backgroundColor = 'green';
+    colorButton.style.color = 'white';
+});
+colorButton.addEventListener('mouseout', () => {
+    colorButton.style.backgroundColor = ''; // Revert back to the default background color
+    colorButton.style.color = ''; // Revert back to the default text color
+  });
+
+    //Event Delegation
+const originalDiv = document.getElementById('original');
+originalDiv.addEventListener('click', (event) => {
+  if (event.target.tagName === 'button') {
+    console.log("Clicked button:", event.target.textContent);
+  }
+});
+
+    //Form Data Extraction
+    document.addEventListener('DOMContentLoaded', function () {
+        const formContainer = document.getElementById('formContainer');
+      
+        const form = document.createElement('form');
+        form.id = 'myForm';
+      
+        const nameLabel = document.createElement('label');
+        nameLabel.textContent = 'Name:';
+        const nameInput = document.createElement('input');
+        nameInput.type = 'text';
+        nameInput.id = 'name';
+        nameInput.name = 'name';
+        nameInput.required = true;
+      
+        const emailLabel = document.createElement('label');
+        emailLabel.textContent = 'Email:';
+        const emailInput = document.createElement('input');
+        emailInput.type = 'email';
+        emailInput.id = 'email';
+        emailInput.name = 'email';
+        emailInput.required = true;
+      
+        const submitButton = document.createElement('button');
+        submitButton.type = 'submit';
+        submitButton.textContent = 'Submit';
+      
+        form.appendChild(nameLabel);
+        form.appendChild(nameInput);
+        form.appendChild(document.createElement('br'));
+        form.appendChild(emailLabel);
+        form.appendChild(emailInput);
+        form.appendChild(document.createElement('br'));
+        form.appendChild(submitButton);
+      
+        formContainer.appendChild(form);
+      
+        form.addEventListener('submit', (event) => {
+          event.preventDefault();
+      
+          const name = document.getElementById('name').value;
+          const email = document.getElementById('email').value;
+      
+          console.log('Name:', name);
+          console.log('Email:', email);
+        });
+      });
