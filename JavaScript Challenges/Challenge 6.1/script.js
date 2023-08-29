505968a007ae2b9d3289396192d41a900e26d31c
Mohamad Safa <mohsafa3@gmail.com>
2023-08-29 10:24:56 +0300

    Challenge 6.1 - Done with step 3

--- JavaScript Challenges/Challenge 6.1/script.js
+++ JavaScript Challenges/Challenge 6.1/script.js
@@ -14,11 +14,21 @@ Delay(5000)
     });
 
 //Step 2: Fetch a Random Fact
-fetch("https://uselessfacts.jsph.pl/random.json?language=en")
+fetch("https://uselessfacts.jsph.pl/random.json?language=en") //Fetch always come with the chain of then and catch below
     .then(response => response.json())
     .then(response => {
-        console.log(response)
+        console.log(response) //response.text will get the text from json only without showing the details in the console log
     })
     .catch(error => {
         console.log("error", error);
     })
+
+//Step 3 : Rewrite with Async/Await
+async function fetchData(){
+    try {
+        const response1 = await fetch ("https://uselessfacts.jsph.pl/random.json?language=en")
+        const data1  = await response1.json();
+        console.log(data1);
+    } catch (error) {
+        console.log("error", error);
+    }};
