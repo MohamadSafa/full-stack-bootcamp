6b84d0efc2079367df7ab5dd185c3bed015cbdc9
Mohamad Safa <mohsafa3@gmail.com>
2023-08-29 10:14:30 +0300

    Challenge 6.1 - Done with step 2

--- JavaScript Challenges/Challenge 6.1/script.js
+++ JavaScript Challenges/Challenge 6.1/script.js
@@ -12,3 +12,13 @@ Delay(5000)
     .then((message) => {
         console.log(message)
     });
+
+//Step 2: Fetch a Random Fact
+fetch("https://uselessfacts.jsph.pl/random.json?language=en")
+    .then(response => response.json())
+    .then(response => {
+        console.log(response)
+    })
+    .catch(error => {
+        console.log("error", error);
+    })
