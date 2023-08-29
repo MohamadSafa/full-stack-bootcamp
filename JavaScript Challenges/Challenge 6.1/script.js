67d373eb5c7c2e73d48597c6c33a6e9a2fe73dba
Mohamad Safa <mohsafa3@gmail.com>
2023-08-29 10:42:19 +0300

    Challenge 6.1 - Done with step 4

--- JavaScript Challenges/Challenge 6.1/script.js
+++ JavaScript Challenges/Challenge 6.1/script.js
@@ -26,9 +26,32 @@ fetch("https://uselessfacts.jsph.pl/random.json?language=en") //Fetch always com
 //Step 3 : Rewrite with Async/Await
 async function fetchData(){
     try {
-        const response1 = await fetch ("https://uselessfacts.jsph.pl/random.json?language=en")
-        const data1  = await response1.json();
-        console.log(data1);
+        let response1 = await fetch ("https://uselessfacts.jsph.pl/random.json?language=en");
+        let data1  = await response1.json();
+        console.log(data1.text) // Here text will show in the console not the file with details because of .text
+    } catch (error) {
+        console.error("error", error);
+    }};
+fetchData();
+
+//Step 4: Fetch Multiple Dog Images 
+const urls = [
+    "https://dog.ceo/api/breeds/image/random",
+    "https://dog.ceo/api/breeds/image/random",
+    "https://dog.ceo/api/breeds/image/random"
+];
+const fetchImage = async (url) => {
+    const response = await fetch(url);
+    const data2 = await response.json();
+    return data2.message;
+};
+const fetchImagesFromUrls = async (urls) => {
+    try {
+        const imagePromises = urls.map(url => fetchImage(url));
+        const images = await Promise.all(imagePromises);
+        console.log(images);
     } catch (error) {
         console.log("error", error);
-    }};
+    }
+    };
+fetchImagesFromUrls(urls);
